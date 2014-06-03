13527f269659cba738ef14c05140caee3a58c732
MehdiMhalla <mehdi.mhalla@gmail.com>
2014-06-03 13:55:10 +0200

    try more null and verif

--- src/search.cpp
+++ src/search.cpp
@@ -592,7 +592,7 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         && !ss->skipNullMove
-        &&  depth >  3 * ONE_PLY /2    +   6 * (int(eval - beta) / PawnValueMg * ONE_PLY)
+        &&  depth >  3 * ONE_PLY /2    +   4 * (int(eval - beta) / PawnValueMg * ONE_PLY)
         &&  eval >= beta
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY
         &&  pos.non_pawn_material(pos.side_to_move()))
@@ -619,7 +619,7 @@ namespace {
             if (nullValue >= VALUE_MATE_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (depth < 12 * ONE_PLY)
+            if (depth < 6 * ONE_PLY)
                 return nullValue;
 
             // Do verification search at high depths
